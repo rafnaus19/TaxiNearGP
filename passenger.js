f0b09f22fadcb202db7d231f41a2fdaca6cc65f0
rafnaus19 <rafn.aus19@gmail.com>
2026-01-22 16:01:15 +0500

    Add files via upload

--- passenger.js
+++ passenger.js
@@ -1,36 +1,35 @@
-import { db } from "./firebase.js";
-import {
-  ref, set, remove, onValue, serverTimestamp
-} from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
-
 let map, myMarker, requestId = null;
 
+// Init map
 navigator.geolocation.getCurrentPosition(pos => {
   map = L.map("map").setView([pos.coords.latitude, pos.coords.longitude], 15);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
-  myMarker = L.circle([pos.coords.latitude, pos.coords.longitude], { radius: 30 }).addTo(map);
+  myMarker = L.circle([pos.coords.latitude, pos.coords.longitude], {radius:30, color:'blue'}).addTo(map);
 });
 
-hailBtn.onclick = () => {
+// Hail taxi
+document.getElementById("hailBtn").onclick = () => {
   if (requestId) return alert("Already requested");
   requestId = "req_" + Date.now();
   navigator.geolocation.getCurrentPosition(pos => {
-    set(ref(db, "requests/" + requestId), {
+    firebase.database().ref("requests/"+requestId).set({
       lat: pos.coords.latitude,
       lng: pos.coords.longitude,
-      time: serverTimestamp()
+      time: Date.now()
     });
   });
 };
 
-cancelBtn.onclick = () => {
+// Cancel request
+document.getElementById("cancelBtn").onclick = () => {
   if (requestId) {
-    remove(ref(db, "requests/" + requestId));
+    firebase.database().ref("requests/"+requestId).remove();
     requestId = null;
   }
 };
 
-onValue(ref(db, "drivers"), snap => {
+// Show online drivers
+firebase.database().ref("drivers").on("value", snap => {
   snap.forEach(d => {
     const v = d.val();
     L.marker([v.lat, v.lng]).addTo(map)
@@ -38,10 +37,10 @@ onValue(ref(db, "drivers"), snap => {
   });
 });
 
-onValue(ref(db, "requests"), snap => {
-  snap.forEach(r => {
-    if (r.val().driver) {
-      document.getElementById("acceptedSound").play();
-    }
-  });
+// Play sound if driver accepts request
+firebase.database().ref("requests").on("child_changed", snap => {
+  const r = snap.val();
+  if (r.driver && requestId) {
+    document.getElementById("acceptedSound").play();
+  }
 });
